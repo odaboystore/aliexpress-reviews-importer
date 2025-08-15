821c1eb9fd2a6b0e462e420506191fac6cf994df
odaboystore <odaboystore@gmail.com>
2025-08-15 16:10:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.get("/ping", (req, res) => {
   res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en puerto ${PORT}`);
 });
